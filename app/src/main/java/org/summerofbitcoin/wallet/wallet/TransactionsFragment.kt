bc335c7befbae41a8ef20fa8f4a808f976b8d728
thunderbiscuit <thunderbiscuit@protonmail.com>
2021-07-07 17:18:54 -0400

    Feat: Add transaction history data

--- app/src/main/java/org/summerofbitcoin/wallet/wallet/TransactionsFragment.kt
+++ app/src/main/java/org/summerofbitcoin/wallet/wallet/TransactionsFragment.kt
@@ -6,12 +6,15 @@
 package org.summerofbitcoin.wallet.wallet
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import org.bitcoindevkit.bdkjni.Types.TransactionDetails
 import org.summerofbitcoin.wallet.R
+import org.summerofbitcoin.wallet.data.Wallet
 import org.summerofbitcoin.wallet.databinding.FragmentTransactionsBinding
 
 class TransactionsFragment : Fragment() {
@@ -30,10 +33,24 @@ class TransactionsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val navController = Navigation.findNavController(view)
+        binding.transactionsView.text = transactionList()
 
+        val navController = Navigation.findNavController(view)
         binding.transactionsToWalletButton.setOnClickListener {
             navController.navigate(R.id.action_transactionsFragment_to_walletFragment)
         }
+    }
+
+    private fun transactionList(): String {
+        val rawList: List<TransactionDetails> = Wallet.listTransactions()
+        var finalList: String = ""
+        for (item in rawList) {
+            Log.i("SobiWallet", "Transaction list item: $item")
+            val transactionInfo: String =
+                "Timestamp: ${item.timestamp}\nReceived: ${item.received}\nSent: ${item.sent}\nFees: ${item.fees}\nTxid: ${item.txid}"
+
+            finalList = "$finalList\n$transactionInfo\n"
+        }
+        return finalList
     }
 }
